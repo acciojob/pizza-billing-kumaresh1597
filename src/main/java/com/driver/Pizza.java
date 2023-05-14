7745bb8a4ceeb0966a0ad0a970f7345e6e148c33
kumaresh1597 <kumaresh.1597@gmail.com>
2023-05-14 16:05:02 +0530

    first

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,32 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int total;
+    private int toppings;
+    final int vegBasePrice = 300;
+    final int nonVegBasePrice = 400;
+    final int cheesePrice = 80;
+    final int toppingsPriceVeg = 70;
+    final int toppingsPriceNonVeg = 120;
+    final int bagPrice = 20;
+
+    private boolean cheeseAdded = false;
+    private boolean toppingsAdded = false;
+    private boolean bagAdded = false;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = vegBasePrice;
+            toppings = toppingsPriceVeg;
+            total += vegBasePrice;
+        }
+        else{
+            price = nonVegBasePrice;
+            toppings = toppingsPriceNonVeg;
+            total += nonVegBasePrice;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +39,37 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!cheeseAdded){
+            total += cheesePrice;
+            cheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!toppingsAdded){
+            total += toppings;
+            toppingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!bagAdded){
+            total += bagPrice;
+            bagAdded = true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        bill = "Base Price Of The Pizza: "+price+"\n";
+        if(cheeseAdded)
+        bill = bill + "Extra Cheese Added: "+cheesePrice+"\n";
+        if(toppingsAdded)
+        bill = bill + "Extra Toppings Added: "+toppings+"\n";
+        if(bagAdded)
+        bill = bill + "Paperbag Added: "+bagPrice+"\n";
+
+        bill = bill + "Total Price: "+total;
+
         return this.bill;
     }
 }
